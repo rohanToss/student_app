000be65706e85c799c371a94b1f2dbb4680c7150
rohanToss <rohantestrocky94@gmail.com>
2020-12-11 16:50:42 +0530

    update password and forgot password route added

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
 const user = require('../controllers/users');
+const auth = require('../config/auth');
 
 /**
  * REGISTER A NEW USER
  */
 router.post('/register',user.register);
 /**
- * VERIFY THE REGISTERED USER
+ * VERIFY THE REGISTERED USE
  */
-router.get('/:id',user.verify);
+router.get('/:id',user.verify); // verify the user registered
 
 /**
  * LOGIN ROUTE
  */
 router.post('/login',user.login);
 
+/**
+ * UPDATE PASSWORD
+ */
+router.post('/password-update',auth.authenticateToken,user.updatePassword);
 
+/**
+ * FORGOT PASSWORD
+ */
+ router.post('/password-forgot',user.forgotPassword);
 
 module.exports = router;
