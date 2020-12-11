f9a1b0b81a3b62a6c73685bb9c8b774c2d298906
rohanToss <rohantestrocky94@gmail.com>
2020-12-11 12:42:02 +0530

    login route added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const user = require('../controllers/users');
 
-
+/**
+ * REGISTER A NEW USER
+ */
 router.post('/register',user.register);
+/**
+ * VERIFY THE REGISTERED USER
+ */
 router.get('/:id',user.verify);
+
+/**
+ * LOGIN ROUTE
+ */
+router.post('/login',user.login);
+
 
 
 module.exports = router;
